4a5d36dd6166209fbf877d44664d45f3ed6a6d86
EldiKab <eldikabashi1@gmail.com>
2023-06-12 21:29:09 +0200

    detailsCtrl by Eldi

--- code/Site/js/detailsCtrl.js
+++ code/Site/js/detailsCtrl.js
@@ -7,9 +7,14 @@ class DetailsCtrl {
         indexCtrl.loadJeux();
     });
     http.getJeuAvecId(id, (jeu)=>{
-        $("#titreJeu").value=jeu.title;
-        $("#descriptionJeu").value=jeu.description;
+        $("#titreJeu").text(jeu.title);
+        $("#descriptionJeu").text(jeu.description);
+        $("#genreJeu").text(jeu.genre);
+        $("#plateformeJeu").text(jeu.platform);
+        $("#developpeursJeu").text(jeu.developer);
+        $("#datesortieJeu").text(jeu.release_date);
+        $("<img src='" + jeu.thumbnail+"'</img>").appendTo("#imageJeu");
+        
     });
   }
-
 }
